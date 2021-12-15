80f133fa18e4107675052e2c8774ae08e74c5ef5
deepan <deepangtm2000@gmail.com>
2021-12-15 11:31:59 +0530

    .

---
+++ src/test/java/com/capg/hollywood/HollywoodConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.hollywood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollywoodConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
